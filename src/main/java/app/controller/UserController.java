c0173a98c581956e6e927873d949f8fbb345b2cd
ric <ric7705@gmail.com>
2019-05-14 14:09:44 +0800

    add application.properties

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -2,6 +2,8 @@ package app.controller;
 
 import app.service.UserService;
 import app.vo.UserVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,9 +17,13 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(method = RequestMethod.GET)
     public List<UserVo> getUsers(){
+
+        LOG.info("get users");
+
         return userService.getUsers();
     }
 }
